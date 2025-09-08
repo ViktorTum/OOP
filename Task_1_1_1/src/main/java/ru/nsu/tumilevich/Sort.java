5a64a1d653dbf99fad0e765c3acb54d2aad66da7
TheNemoHuan <vicktortumilevich@gmail.com>
2025-09-08 23:31:29 +0700

    22

--- Task_1_1_1/src/main/java/ru/nsu/tumilevich/Sort.java
+++ Task_1_1_1/src/main/java/ru/nsu/tumilevich/Sort.java
@@ -1,20 +1,20 @@
 package ru.nsu.tumilevich;
+import java.util.Arrays;
 
 /**
  * Sample class to simulate 1.1 task functionality
  */
 public class Sort {
     public static Integer[] pyrsort(Integer[] array) {
-            // Сортировка всего массива
-            Arrays.sort(array);
-            System.out.println(Arrays.toString(array)); // [1, 2, 3, 5, 8]
+        // Сортировка всего массива
+        Arrays.sort(array);
+        return array; // [1, 2, 3, 5, 8]
 
-            // Сортировка части массива (от индекса 1 до 3)
-            Integer [] partialArray = {5, 2, 8, 1, 3};
-            Arrays.sort(partialArray, 1, 4);
-            return partialArray; // [5, 1, 2, 8, 3]
-        }
+    }
 
+    public static void main(String[] args) {
+        Integer [] partialArray = {5, 2, 8, 1, 3};
+        System.out.println(Arrays.toString(pyrsort(partialArray)));
     }
 
 }
