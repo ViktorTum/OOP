a728460472151145e6416dda508ece97b617bc9c
NemoHuan <v.tumilevich@g.nsu.ru>
2025-11-24 02:47:20 +0700

    add tests

--- Task_1_2_2/build.sh
+++ Task_1_2_2/build.sh
@@ -1,3 +1,3 @@
-javac -d . src/main/java/ru/nsu/tumilevich/Sort.java
-javadoc -d docs src/main/java/ru/nsu/tumilevich/Sort.java
-java ru.nsu.tumilevich.Sort
+javac -d . src/main/java/ru/nsu/tumilevich/HashTable.java
+javadoc -d docs src/main/java/ru/nsu/tumilevich/HashTable.java
+java ru.nsu.tumilevich.HashTable
